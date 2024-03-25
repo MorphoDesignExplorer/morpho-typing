860a58055439653a800428c92d19d3a5a9fa77fe
Krishna Sivakumar <krishnasivaprogrammer@gmail.com>
2024-03-25 18:24:38 -0400

    moved docstring from types.py to __init__.py

--- src/morpho_typing/types.py
+++ src/morpho_typing/types.py
@@ -1,22 +1,3 @@
-"""
-This module provides a set of schemas common to the Morpho Design Explorer Server and GA-Search Client.
-
-There are two schemas available as of now:
-1. MorphoProjectSchema: defines and provides validation for the general structure of a project's schema
-2. MorphoAssetCollection: defines a set of assets / files that are associated with a schema.
-
-Project Usage:
-
-# to validate a project schema definition
-schema = MorphoProjectSchema(values=[...])
-
-# to validate a record against an existing project schema object
-schema.validate_record(record)
-
-# to validate an asset collection definition
-asset_schema = MorphoAssetCollection(assets=[...])
-"""
-
 from enum import StrEnum
 from typing_extensions import Annotated, Literal
 import pydantic
